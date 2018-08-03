e0d367e7d2d3272d5c02b4ab949b2580c80ceffb
AlinPopa <alin1popa@gmail.com>
2018-08-03 16:36:54 +0300

    filling orders

--- ethereum/FlowCoin.sol
+++ ethereum/FlowCoin.sol
@@ -143,6 +143,82 @@ contract FlowCoin is StandardToken {
         return true;
     }
     
+    /**
+     * @dev Fill one particular buy order from the order book
+     * @param _ratio the ratio of FLOW-to-ETH
+     * @param _amount the amount of tokens to be sold
+     * @param _buyer the address of the buyer
+     */
+    function fillBuyOrder(
+        uint256 _ratio,
+        uint256 _amount,
+        uint256 _buyer
+    )   private
+        returns (uint256)
+    {
+        uint256 ethToTransfer;
+        uint256 flowToTransfer;
+        
+        bytes32 orderHash = keccak256(abi.encodePacked(_ratio, _buyer));
+        if (buys[orderHash] >= _amount) {
+            flowToTransfer = _amount;
+        } else {
+            flowToTransfer = buys[orderHash];
+        }
+        
+        if (flowToTransfer != 0) {
+            ethToTransfer = flowToTransfer.mul(_ratio);
+            
+            buys[orderHash] = buys[orderHash].sub(flowToTransfer);
+            balances[msg.sender] = balances[msg.sender].sub(flowToTransfer);
+            balances[_buyer] = balances[_buyer].add(flowToTransfer);
+            
+            msg.sender.transfer(ethToTransfer);
+            
+            emit Buy(_ratio, buys[orderHash], _buyer);
+            emit Transfer(msg.sender, _buyer, flowToTransfer);
+        }
+        
+        return _amount - flowToTransfer;
+    }
     
+    /**
+     * @dev Fill one particular sell order from the order book
+     * @param _ratio the ratio of FLOW-to-ETH
+     * @param _amount the amount of tokens to be bought
+     * @param _seller the address of the seller
+     */
+    function fillSellOrder(
+        uint256 _ratio,
+        uint256 _amount,
+        uint256 _seller
+    )   private
+        returns (uint256)
+    {
+        uint256 ethToTransfer;
+        uint256 flowToTransfer;
+        
+        bytes32 orderHash = keccak256(abi.encodePacked(_ratio, _seller));
+        if (sells[orderHash] >= _amount) {
+            flowToTransfer = _amount;
+        } else {
+            flowToTransfer = sells[orderHash];
+        }
+        
+        if (flowToTransfer != 0) {
+            ethToTransfer = flowToTransfer.mul(_ratio);
+            
+            sells[orderHash] = sells[orderHash].sub(flowToTransfer);
+            balances[_seller] = balances[_seller].sub(flowToTransfer);
+            balances[msg.sender] = balances[msg.sender].add(flowToTransfer);
+            
+            _seller.transfer(ethToTransfer);
+            
+            emit Sell(_ratio, sells[orderHash], _seller);
+            emit Transfer(_seller, msg.sender, flowToTransfer);
+        }
+        
+        return _amount - flowToTransfer;
+    }
 }
  
