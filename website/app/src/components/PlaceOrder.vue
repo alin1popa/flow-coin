40e8caf787f9168bda64721d71aafcc3182e28f6
Alin Popa <alin.popa@softwire.com>
2019-11-05 14:46:26 +0200

    Bind models to form in place order

--- website/app/src/components/PlaceOrder.vue
+++ website/app/src/components/PlaceOrder.vue
@@ -4,12 +4,12 @@
     {{ state.val }}
 
     <div v-if=true>
-      <input type="number" name="ratio"/>
-      <input type="number" name="amount"/>
-      <input type="radio" name="type" value="buy"/>
-      <input type="radio" name="type" value="sell"/>
+      <input type="number" name="ratio" v-model="ratio"/>
+      <input type="number" name="amount" v-model="amount"/>
+      <input type="checkbox" name="type" v-model="type"/>
 
-      <button name="submit" value="submit">Place {{ balance }} Order</button>
+      {{ disclaimerText }}
+      <button name="submit" value="submit" v-bind:disabled="!isLoading" v-on:click="submitOrder()">{{ isLoading ? 'Place Order' : 'Pending...'}}</button>
     </div>
 
   </div>
@@ -22,6 +22,8 @@ import { Order } from '@/models/Order';
 import { OrderType } from '@/constants/OrderType';
 import { ContractService } from '@/services/ContractService';
 import { StateManager } from '@/services/StateManager';
+import { Request } from '@/models/Request';
+import { RequestType } from '../constants/RequestType';
 
 @Component<PlaceOrder>({
   mounted() {
@@ -34,17 +36,42 @@ import { StateManager } from '@/services/StateManager';
     });
   },
 })
+
+enum VIEW_ORDER_TYPE {
+  BUY = 1,
+  SELL = 0
+}
+
 export default class PlaceOrder extends Vue {
+  private ratio: number = 1;
+  private amount: number = 1;
+  private type: VIEW_ORDER_TYPE = VIEW_ORDER_TYPE.BUY;
+  private isLoading: boolean = false;
+
   private info: string = '';
 
   private state = StateManager.GetInstance().GetState();
+
+  get disclaimerText(): string {
+    return `I want to ${this.type === VIEW_ORDER_TYPE.BUY ? 'buy' : 'sell'} ${this.amount} of FC at ${this.ratio} ethers each`
+  }
 
   get balance(): number {
     return ContractService.GetBalance();
   }
 
-  public actionClicked(order: Order) {
-    alert(JSON.stringify(order));
+  public submitOrder() {
+    this.isLoading = true;
+
+    const request = new Request(
+      this.type === VIEW_ORDER_TYPE.BUY ? OrderType.BUY : OrderType.SELL,
+      RequestType.REGULAR,
+      this.amount,
+      this.ratio
+    );
+
+    ContractService.PlaceOrderRequest(request);
+    this.isLoading = false; // TODO this should be a .then()
   }
 }
 </script>
